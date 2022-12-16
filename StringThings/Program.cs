e31b7315b3d2a925449677551ca226ede9b5c9a7
Jasper Kent <software@jasperkent.com>
2022-12-16 08:36:28 +0000

    Add project files.

---
+++ StringThings/Program.cs
@@ -0,0 +1,23 @@
+﻿if (true)
+{
+    int x = 666;
+
+    var html = $"""""
+                <!DOCTYPE html>
+                <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+                    <head>
+                        <meta charset=\"utf-8\" />
+                        <title>My Page</title>
+                    </head>
+                    <body>
+                        <h1>This is a heart symbol: "\u2665"</h1>
+                        <p>In C# 11 we can use """"</p>
+                        <p>The value is {x}</p>
+                    </body>
+                </html>
+                """"";
+
+    using var writer = new StreamWriter("output.html", false);
+
+    writer.WriteLine(html);
+}
